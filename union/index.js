a995a7224a74861a962f1c0a84bd1c354354e9d2
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-10-05 23:06:16 +0300

    Add Union examples and Type Narrowing

---
+++ union/index.js
@@ -0,0 +1,28 @@
+"use strict";
+function logId(id) {
+    if (typeof id === "string") {
+        console.log(id);
+    }
+    else if (typeof id === "number") {
+        console.log(id);
+    }
+    else {
+        console.log(id);
+    }
+}
+function logError(err) {
+    if (Array.isArray(err)) {
+        console.log(err);
+    }
+    else {
+        console.log(err);
+    }
+}
+function logObject(obj) {
+    if ("a" in obj) {
+        console.log(obj.a);
+    }
+    else {
+        console.log(obj.b);
+    }
+}
