a965e9501f611a8caa5388e95b3ac5f8d4a8782f
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-10-05 23:07:44 +0300

    Add Type and Interface examples

---
+++ type and interface/index.ts
@@ -0,0 +1,47 @@
+type User = {
+  name: string;
+  age: number;
+  skills: string[];
+};
+
+type TRole = {
+  name: string;
+  id: number;
+};
+
+// type UserWithRole = User & TRole;
+
+type UserWithRole = {
+  user: User;
+  role: TRole;
+};
+
+interface IUser {
+  name: string;
+  age: number;
+  skills: string[];
+
+  log?: (id: number) => string;
+}
+
+interface IRole {
+  roleId: number;
+}
+
+interface IUserWithRole extends IUser, IRole {
+  createdAt: Date;
+}
+
+interface UserDic {
+  [index: number]: IUser;
+}
+// Аналог
+type ud = Record<number, IUser>;
+
+const userDicObj: UserDic = {
+  1: {
+    name: "Misha",
+    age: 30,
+    skills: ["JS", "Java"],
+  },
+};
