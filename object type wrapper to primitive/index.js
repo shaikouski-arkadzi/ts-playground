945fab75b78e286e5f5e901478c719a2b7bf507f
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-10-11 23:06:14 +0300

    Add object type wrapper to primitive

---
+++ object type wrapper to primitive/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 5;
+//String to string
+let e = new String(a).valueOf();
+let f = new Boolean(a).valueOf();
---
+++ object type wrapper to primitive/index.ts
@@ -0,0 +1,7 @@
+let a = 5;
+
+//String to string
+let e: string = new String(a).valueOf();
+let f: boolean = new Boolean(a).valueOf();
+
+export {};
