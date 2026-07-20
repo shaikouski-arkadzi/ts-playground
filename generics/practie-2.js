76f17e98cde3d16d59f9dd332c3a159b1db6864a
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-07-20 21:46:02 +0300

    Add generic practice 2

---
+++ generics/practie-2.js
@@ -0,0 +1,21 @@
+"use strict";
+/*
+  Необходимо написать функцию сортировки любых объектов,
+  которые имеют id
+  по убыванию и по возрастанию
+*/
+const data = [
+    { id: 1, name: "Вася" },
+    { id: 2, name: "Петя" },
+    { id: 3, name: "Надя" },
+];
+function sort(data, type = "asc") {
+    return data.sort((a, b) => {
+        switch (type) {
+            case "asc":
+                return a.id - b.id;
+            case "desc":
+                return b.id - a.id;
+        }
+    });
+}
---
+++ generics/practie-2.ts
@@ -0,0 +1,26 @@
+/* 
+  Необходимо написать функцию сортировки любых объектов,
+  которые имеют id
+  по убыванию и по возрастанию
+*/
+
+const data = [
+  { id: 1, name: "Вася" },
+  { id: 2, name: "Петя" },
+  { id: 3, name: "Надя" },
+];
+
+interface ID {
+  id: number;
+}
+
+function sort<T extends ID>(data: T[], type: "asc" | "desc" = "asc"): T[] {
+  return data.sort((a, b) => {
+    switch (type) {
+      case "asc":
+        return a.id - b.id;
+      case "desc":
+        return b.id - a.id;
+    }
+  });
+}
