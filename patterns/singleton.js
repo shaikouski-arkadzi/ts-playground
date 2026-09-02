e1036f1404904f2371100df50f5bae43eeadbbd0
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-09-02 22:49:14 +0300

    Add singleton pattern example

---
+++ patterns/singleton.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class MyMap {
+    constructor() {
+        this.map = new Map();
+    }
+    clean() {
+        this.map = new Map();
+    }
+    static get() {
+        if (!MyMap.instance) {
+            MyMap.instance = new MyMap();
+        }
+        return MyMap.instance;
+    }
+}
+class Service1 {
+    addMap(key, value) {
+        const myMap = MyMap.get();
+        myMap.map.set(key, value);
+    }
+}
+class Service2 {
+    getKeys(key) {
+        const myMap = MyMap.get();
+        console.log(myMap.map.get(key));
+    }
+}
+new Service1().addMap(1, "works");
+new Service2().getKeys(1);
